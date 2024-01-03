af23be4c45f20814141c1c4b4201570bdd022548
Samuel R Borges <sammy_rb@hotmail.com>
2024-01-03 08:29:57 -0300

    Created endPoint GET Client by id

--- src/main/java/com/samucadev/desafioclient/controllers/ClientController.java
+++ src/main/java/com/samucadev/desafioclient/controllers/ClientController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -23,5 +21,11 @@ public class ClientController {
     public Page<ClientDTO> findAll(Pageable pageable) {
         Page<ClientDTO> dto = service.findAll(pageable);
         return dto;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 }
