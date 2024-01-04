612b5a9d1c0a232a7b38edffb2d62bc8d9c3207a
Samuel R Borges <sammy_rb@hotmail.com>
2024-01-03 21:41:26 -0300

    added alternative method to insert client inside controller

--- src/main/java/com/samucadev/desafioclient/controllers/ClientController.java
+++ src/main/java/com/samucadev/desafioclient/controllers/ClientController.java
@@ -5,6 +5,7 @@ import com.samucadev.desafioclient.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +33,7 @@ public class ClientController {
         return ResponseEntity.ok(dto);
     }
 
+    /* //utilizando responseEntity
     @PostMapping
     public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
         dto = service.insert(dto);
@@ -39,4 +41,21 @@ public class ClientController {
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+    */
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClientDTO insert(@RequestBody ClientDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uri);
+
+        return dto;
+    }
+
+
+
 }
